cd7d0e7c57cd9d425b12c2820b3ad6413aa48a20
Kelly McBride <kemcbride28@gmail.com>
2017-08-30 22:37:42 -0400

    Moved map stuff into State, also added a 'mode' thing...

--- neolib/neoquest/State.py
+++ neolib/neoquest/State.py
@@ -5,7 +5,11 @@
 .. moduleauthor:: Kelly McBride <kemcbride28@gmail.com>
 """
 
+from munch import Munch
 import re
+import math
+import logging
+
 
 class State(object):
     """So this object should kind of keep-alive your progress/game state
@@ -16,7 +20,27 @@ class State(object):
     """
     OVERWORLD_REGEX = '\s*NeoQuest\s*(Name.*Sneaking)\s*Options\s*|\s*Leader\s*Board\s*(.*)'
     BATTLE_SPLASH_REGEX = '\s*NeoQuest\s*(.*)(You are.*)'
+    END_FIGHT_REGEX = '\s*NeoQuest\s*(.*)(You defeated.*)'
     BATTLE_REGEX = '\s*NeoQuest\s*(\w*.*Difficulty: [A-Z][a-z]+)(.*)<.*>(.*)'
+    ASCII_MAP = {
+        'grassland': '.',
+        'mountain': '^',
+        'swamp': 'w',
+        'forest': 'T',
+        'city': 'X',
+        'castle': '#',
+        'hills': 'h',
+        'desert': '_',
+        'lupe': '@',
+        'water': ' ',
+        }
+    MODES = Munch({
+        'OVERWORLD': 'OVERWORLD',
+        'MENU': 'MENU',
+        'BATTLE': 'BATTLE',
+        'TRANSITION': 'TRANSITION',
+        })
+
 
     def __init__(self, page):
         """State is constructed from a Page"""
@@ -29,10 +53,16 @@ class State(object):
         self.raw_text = source_div.get_text().encode('ascii', 'ignore')
         if 'attacked by' in self.raw_text:
             regex = State.BATTLE_SPLASH_REGEX
+            self.mode = State.MODES.TRANSITION
+        elif 'defeated' in self.raw_text:
+            regex = State.END_FIGHT_REGEX
+            self.mode = State.MODES.TRANSITION
         elif 'Attack' in self.raw_text:
             regex = State.BATTLE_REGEX
+            self.mode = State.MODES.BATTLE
         else:
             regex = State.OVERWORLD_REGEX
+            self.mode = State.MODES.OVERWORLD
         self.match_data = re.findall(regex, re.sub('\n', ' ', self.raw_text), flags=re.DOTALL)
 
         # Clean up the results
@@ -51,3 +81,42 @@ class State(object):
 
     def __repr__(self):
         return str(self)
+
+    def _create_ascii_map(self):
+        def url_to_ascii(img_url):
+            terrain = img_url.split('/')[-1] # Remove path
+            terrain = terrain.split('.')[0] # Remove extension
+            if terrain.startswith('lupe_'):
+                terrain = 'lupe'
+            if 'water' in terrain: # untraversable terrain is highest pri?
+                terrain = 'water'
+            return State.ASCII_MAP.get(terrain, '?')
+
+        """For debugging and console fun, let's create a nethack-kinda map!"""
+        tiles = [
+                i.attrs['src'] for i in
+                self.src_div.find_all('img')
+                if i['height']=='40' and i['width']=='40'
+                ]
+
+        ascii_map = []
+
+        # I *could* hardcode this to 49 and 7 - but what's the fun in that?
+        row_len = int(math.floor(math.sqrt(len(tiles))))
+        for row_idx in range(len(tiles)/row_len):
+            ascii_map.append(map(
+                url_to_ascii,
+                tiles[row_idx*row_len:(row_idx+1)*row_len]
+                ))
+        return ascii_map
+
+    def _print_ascii_map(self, ascii_map):
+        for row in ascii_map:
+            print ''.join(row)
+
+    def map(self):
+        if self.mode != State.MODES.OVERWORLD:
+            # TODO: use proper logging
+            logging.warn("Can't create ascii map when not in overworld mode")
+            return
+        self._print_ascii_map(self._create_ascii_map())
