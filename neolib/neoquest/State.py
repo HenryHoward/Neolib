fd8094eda2b816ce002fa8bf255f9f529ca41f22
Kelly McBride <kemcbride28@gmail.com>
2017-08-31 15:19:57 -0400

    You can do local actions, relatively reliably...

--- neolib/neoquest/State.py
+++ neolib/neoquest/State.py
@@ -10,6 +10,14 @@ import re
 import math
 import logging
 
+from Action import Action
+
+def is_br(bs_tag):
+    try:
+        return bs_tag.is_empty_element
+    except AttributeError:
+        return False
+
 
 class State(object):
     """So this object should kind of keep-alive your progress/game state
@@ -37,6 +45,7 @@ class State(object):
     MODES = Munch({
         'OVERWORLD': 'OVERWORLD',
         'MENU': 'MENU',
+        'TALK': 'TALK',
         'BATTLE': 'BATTLE',
         'TRANSITION': 'TRANSITION',
         })
@@ -48,39 +57,99 @@ class State(object):
                 "div", class_="contentModule phpGamesNonPortalView"
                ).find('div', class_='frame')
         self.src_div = source_div
+        self.local_actions = []
 
-        # NOTE: there's a single grosso unicode char
+        # NOTE: there's a single grosso unicode char (for some state)
         self.raw_text = source_div.get_text().encode('ascii', 'ignore')
         if 'attacked by' in self.raw_text:
             regex = State.BATTLE_SPLASH_REGEX
             self.mode = State.MODES.TRANSITION
+            self.match_data = re.findall(regex, re.sub('\n', ' ', self.raw_text), flags=re.DOTALL)
+
+            # Clean up the results
+            self.data = [a.strip() for m in self.match_data for a in m if a]
         elif 'defeated' in self.raw_text:
             regex = State.END_FIGHT_REGEX
             self.mode = State.MODES.TRANSITION
+            self.match_data = re.findall(regex, re.sub('\n', ' ', self.raw_text), flags=re.DOTALL)
+
+            # Clean up the results
+            self.data = [a.strip() for m in self.match_data for a in m if a]
         elif 'Attack' in self.raw_text:
             regex = State.BATTLE_REGEX
             self.mode = State.MODES.BATTLE
+
+            self.match_data = re.findall(regex, re.sub('\n', ' ', self.raw_text), flags=re.DOTALL)
+
+            # Clean up the results
+            self.data = [a.strip() for m in self.match_data for a in m if a]
+
+        # TODO: is 'talk' screen part of overworld? Not really...
+        elif 'say' in self.raw_text.lower():
+            print("I AM IN THE TALK MODE, so at least there's that")
+            self.mode = State.MODES.TALK
+            self.data, self.local_actions = self._parse_talk(self.src_div)
+
         else:
             regex = State.OVERWORLD_REGEX
             self.mode = State.MODES.OVERWORLD
-        self.match_data = re.findall(regex, re.sub('\n', ' ', self.raw_text), flags=re.DOTALL)
-
-        # Clean up the results
-        self.data = [a.strip() for m in self.match_data for a in m if a]
+            self.data, self.local_actions = self._parse_overworld(self.src_div)
 
     def __str__(self):
-        nice_data = [
-                re.sub('View.*', '', stats)
-                for stats in self.data
-                ]
-        nice_data = [
-                re.sub('\.', '. ', flavor)
-                for flavor in nice_data
-                ]
-        return '\n'.join(nice_data)
+        # nice_data = [
+        #         re.sub('View.*', '', stats)
+        #         for stats in self.data
+        #         ]
+        # nice_data = [
+        #         re.sub('\.', '. ', flavor)
+        #         for flavor in nice_data
+        #         ]
+        return ' '.join(self.data)
 
     def __repr__(self):
         return str(self)
+
+    def _parse_overworld(self, src_div):
+        content_table = src_div.find_all('table')[-1]
+        content_row = content_table.find_all('tr')[-1]
+        content_cells = content_row.find_all('td')
+        content = [list(c.children) for c in content_cells]
+        content = map(lambda l: filter(lambda t: not is_br(t), l), content)
+        content = [item for sublist in content for item in sublist]
+
+        # NOTE/TODO: perhaps, convert the "a" tags into some meaningful little pieces
+        # of data???
+        local_actions = [Action(a) for a in content if a.name=='a']
+        content = map(
+                lambda c: c.text
+                if (not type(c) == unicode and hasattr(c, 'text'))
+                else c,
+                content)
+        return content, local_actions
+
+    def _parse_talk(self, src_div):
+        content = src_div.find('div', attrs={'align':'center'})
+        content = list(content.children)
+        content = filter(lambda t: not is_br(t), content)
+
+        local_actions = [
+                Action(a) for a in content
+                if ((not type(a) == unicode) and (a.name=='a'))
+                ]
+        content = map(
+                lambda c: c.text.encode('ascii', 'ignore')
+                if (not type(c) == unicode and hasattr(c, 'text'))
+                else c,
+                content)
+        return content, local_actions
+
+    def _parse_battle(self, src_div):
+        pass
+
+    # Let's hope 1 function could cover both of these:
+    def _parse_transition(self, src_div):
+        pass
+
 
     def _create_ascii_map(self):
         def url_to_ascii(img_url):
