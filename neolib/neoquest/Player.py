083a33551162cd3ae6923ad5dcad2b88ea8cce04
Kelly McBride <kemcbride28@gmail.com>
2018-01-01 14:40:11 -0500

    Added player module for character stats/hp/etc, stuck it onto NQ object

--- neolib/neoquest/Player.py
+++ neolib/neoquest/Player.py
@@ -12,12 +12,8 @@ import logging
 
 
 class Player(object):
-    """
-    Player should actually encompass things that change less frequently than the state.
-    Stuff that changes less frequently, like HP, MP (?), inventory (?), skill tree, EXP
-    - movement mode? sure, whatever...
-    """
     def __init__(self, state):
+        """src_div as in the same one used for States"""
         self._update(state)
 
 
@@ -26,27 +22,13 @@ class Player(object):
 
 
     def _update(self, state):
-        sdc = [t for t in state.src_div.contents[2].contents if t.name != 'table']
-        self.sdc = sdc
-
+        src_div = state.src_div
+        sdc = src_div.contents[2].contents
         # based on in-battle, the userful player info is like:
-        self.name = sdc[1].string
-        self.level = int(sdc[3].string)
-        self.current_health = int(sdc[5].string)
-        self.max_health = int(sdc[6][1:]) # does NOT need .string because it is a NavigableString
-
-        # NOTE/TODO: This is very weird...
-        # on jan 29, 2018, having rebased on master... this just isn't working, straight up
-        # like, at all
-
-        exp_idx = 11 if len(sdc) >= 12 else 10
-        self.exp = int(filter(lambda c: c.isdigit(), sdc[exp_idx].string))
-
-        # TODO: get this to work consistently
-        # In overworld, it's 16. in battle, it's 17? wth?
-        # could load it once, and not udpate since it doesn't change unless
-        # you explicitly change it.
-        self.difficulty = sdc[16].string if state.mode == 'BATTLE' else sdc[17]
-        # There are separator text elements '|', and if NOT selected, tag is 'a'
-        if state.mode == 'OVERWORLD':
-            self.movement_mode = [t for t in sdc[22:27] if t.name == 'b'][0].string
+        player_stuff = sdc[:17]
+        self.name = sdc[1].text
+        self.level = int(sdc[3].text)
+        self.current_health = int(sdc[5].text)
+        self.max_health = int(sdc[6][1:]) # does NOT need .text because it is a NavigableString
+        self.exp = int(sdc[11].text)
+        self.difficulty = sdc[16].text
