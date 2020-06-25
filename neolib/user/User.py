d32276a1e0abfa13012857da3d8cbeb8d14cc991
HenryHoward <henry.a.c.howard@gmail.com>
2020-06-25 20:33:47 +1000

    on initialisation, a user's session headers now have User-Agent set to blank. This prevents the requests being blocked

--- neolib/user/User.py
+++ neolib/user/User.py
@@ -24,17 +24,17 @@ import hashlib
 import pickle
 
 class User:
-    
+
     """Represents a Neopets user account
-    
+
     The glue of this library, this class represents a Neopets user account
     in the scope of an automated program. It links together many key components
     of the library including Bank, UserShop, SDB, and UserInventory. It allows
     for easy configuration, browser synchronization, and other basic configuration
     like username, password, pin, etc. This class is used as the focal point for
     the rest of the library.
-    
-    
+
+
     Attributes
        username (str) -- User's username
        password (str) -- User's password
@@ -60,24 +60,24 @@ class User:
        configVars (list[str]) -- List of attributes to save with User.exportVars()
        loggedIn (bool) -- Whether or not the user is logged in
        browsers (list[str]) -- List of all installed browsers on the local machine
-       
+
     Initialization
        User(username, password="", pin=None)
-       
+
        Initializes the user with the given details and attempts to load any configuration
-       
+
        Sets the username, password, and pin. Checks if configuration is loaded and attempts
-       to load it if not. Searches the configuration to see if this username has any saved 
+       to load it if not. Searches the configuration to see if this username has any saved
        configuration data. If none is found, creates a section for this username with some
        default settings. If configuration is found, loads the configuration, including
        any exported attribute values. Also loads some default hooks which accomplish
        tasks like auto-login and keeping the activePet and NPs up to date.
-       
+
        Parameters
           username (str) -- User's username
           password (str) -- User's password
           pin (int) -- User's 4 digit pin
-        
+
     Example
        >>> usr = User("username", "password")
        >>> usr.login()
@@ -89,17 +89,17 @@ class User:
        >>> pg.title
        'Welcome to Neopets!'
     """
-    
+
     username = ""
     password = ""
-    
+
     session = None
-    
+
     inventory = None
     bank = None
     shop = None
     SDB = None
-    
+
     nps = 0
     activePet = None
     pin = None
@@ -108,105 +108,110 @@ class User:
     proxy = None
     browser = None
     RECallback = None
-    
+
     lastPage = ""
     useRef = True
     autoLogin = True
     useHooks = True
     browserSync = False
     savePassword = False
-    
+
     configVars = ['username', 'password', 'session', 'proxy', 'browser', 'useRef', 'autoLogin', 'useHooks', 'browserSync', 'savePassword']
-    
+
     def __init__(self, username, password="", pin=None):
         # Neopets automatically converts all capitals in a username to lowercase
         self.username = username.lower()
         self.password = password
         self.pin = pin
-        
+
         # Initialize
         self.inventory = UserInventory(self)
         self.shop = UserShop(self)
         self.bank = Bank(self)
         self.SDB = SDB(self)
-        
+
         # Each User instance needs a unique session
         self.session = Page.newSession()
-        
+
+        # Change 'User-Agent' in the session header to be blank. This is enough
+        #to avoid blocking as of June 2020
+        self.session.headers['User-Agent'] = ""
+
         # Default hooks
         self.hooks = []
         self.hooks.append(updateNPs)
         self.hooks.append(updatePet)
         self.hooks.append(autoLogin)
-        
+
         # Config
         if not Configuration.loaded():
             if Configuration.initialize():
                 self.__loadConfig()
         else:
             self.__loadConfig()
-        
+
     @property
     def loggedIn(self):
         pg = self.getPage("http://www.neopets.com/")
         return self.username in pg.content
-        
+
     @property
     def browsers(self):
         return BrowserCookies.loadBrowsers()
-    
+
     def login(self):
         """ Logs the user in, returns the result
-           
+
         Returns
            bool - Whether or not the user logged in successfully
         """
+
         # Request index to obtain initial cookies and look more human
         pg = self.getPage("http://www.neopets.com")
-        
+
         form = pg.form(action="/login.phtml")
         form.update({'username': self.username, 'password': self.password})
         pg = form.submit()
-		
+
         logging.getLogger("neolib.user").info("Login check", {'pg': pg})
-        
+
         return self.username in pg.content
-    
+
     def addHook(self, hook):
         """ Adds a hook for User.getPage()
         """
         self.hooks.append(hook)
-        
+
     def setRandomEventCallback(self, cb):
         """ Sets the callback function for when a random event is detected
         """
         self.RECallback = cb
-    
+
     def sync(self, browser):
         """ Enables cookie synchronization with specified browser, returns result
-           
+
         Returns
            bool - True if successful, false otherwise
         """
         BrowserCookies.loadBrowsers()
         if not browser in BrowserCookies.browsers:
                 return False
-        
+
         self.browserSync = True
         self.browser = browser
         return True
-        
+
     def deSync(self):
         """ Disables browser synchronization
         """
         self.browserSync = False
-        
+
     def save(self):
         """ Exports all user attributes to the user's configuration and writes configuration
-        
+
         Saves the values for each attribute stored in User.configVars
         into the user's configuration. The password is automatically
-        encoded and salted to prevent saving it as plaintext. The 
+        encoded and salted to prevent saving it as plaintext. The
         session is pickled, encoded, and compressed to take up
         less space in the configuration file. All other attributes
         are saved in plain text. Writes the changes to the configuration file.
@@ -215,7 +220,7 @@ class User:
         for prop in dir(self):
             if getattr(self, prop) == None: continue
             if not prop in self.configVars: continue
-            
+
             # Special handling for some attributes
             if prop == "session":
                 pic = pickle.dumps(getattr(self, prop).cookies)
@@ -223,23 +228,23 @@ class User:
                 enc = base64.b64encode(comp)
                 self.config[prop] = enc.decode()
                 continue
-                    
+
             if prop == "password" and not self.savePassword: continue
             if prop == "password":
                 s = hashlib.md5(self.username.encode()).hexdigest()
                 p = base64.b64encode(getattr(self, prop).encode()) + s.encode()
                 self.config[prop] = p.decode()
                 continue
-                
+
             self.config[prop] = str(getattr(self, prop))
-                
+
         if 'password' in self.config and not self.savePassword: del self.config.password
         self.config.write()
         self.__loadConfig()
-        
+
     def getPage(self, url, postData = None, vars = None, usePin = False):
         """ Requests and returns a page using the user's session
-        
+
         If useRef is set to true, automatically appends a referer using
         the user's last page to the request. If usePin is set to true,
         automatically appends the user's pin to the POST data. If browser
@@ -248,62 +253,62 @@ class User:
         updated cookies to the browser's cookie database. If useHooks is
         set to true, delivers the resulting Page object to each hook
         function for processing. Finally, returns the requested page.
-        
+
         Parameters
            url (str) -- URL of remote page
            postData (dict) -- POST data to send with request
            vars (dict) -- Additional HTTP Header variables to send with request
            usePin (bool) -- Whether or not to send the user's pin with the request
-           
+
         Returns
            Page -- Requested page
-           
+
         Raises
            neopetsOfflineException
         """
-        # If using a referer is desired and one has not already been supplied, 
+        # If using a referer is desired and one has not already been supplied,
         # then set the referer to the user's last visited page.
         if self.useRef and len(self.lastPage) > 0:
             if not vars: vars = {'Referer': self.lastPage}
             elif not "Referer" in vars: vars['Referer'] = self.lastPage
-            
+
         self.lastPage = url
-        
+
         if usePin:
             if self.pin:
                 # All forms that require a pin share the same variable name of 'pin'
                 postData['pin'] = str(self.pin)
-        
+
         if bool(self.browserSync):
             self.__syncCookies()
-        
+
         pg = Page(url, usr=self, postData=postData, vars=vars, proxy=self.proxy)
-        
+
         if self.browserSync:
             self.__writeCookies()
-        
+
         if "http://images.neopets.com/homepage/indexbak_oops_en.png" in pg.content:
             raise neopetsOfflineException
-        
+
         if self.useHooks:
             for hook in self.hooks:
                 self, pg = hook(self, pg)
         return pg
-        
+
     def __syncCookies(self):
-        if self.browserSync:  
+        if self.browserSync:
             cj = BrowserCookies.getCookies(self.browser, ".neopets.com")
             if not cj:
                 return False
-                
+
             self.session.cookies = cj
             return True
         return False
-        
+
     def __writeCookies(self):
-        if self.browserSync:  
+        if self.browserSync:
             BrowserCookies.setCookies(self.browser, ".neopets.com", self.session.cookies)
-            
+
     def __loadConfig(self):
         c = Configuration.getConfig()
         if not self.username in c.users:
@@ -311,16 +316,16 @@ class User:
             self.config = c.users[self.username]
             self.save()
             return
-            
+
         self.config = c.users[self.username]
         for key in self.config:
             if not key in self.configVars: continue
-            
+
             if self.config[key] == 'True':
                 self.config.__dict__[key] = True
             elif self.config[key] == 'False':
                 self.config.__dict__[key] = False
-                
+
             # Special handling for some attributes
             if key == "session":
                 enc = base64.b64decode(self.config.__dict__[key].encode())
@@ -328,17 +333,17 @@ class User:
                 pic = pickle.loads(comp)
                 self.session.cookies = pic
                 continue
-                
+
             if key == "password":
                 s = hashlib.md5(self.username.encode()).hexdigest()
                 p = base64.b64decode(self.config.__dict__[key].replace(s, "").encode())
                 setattr(self, key, p.decode())
                 continue
-            
+
             setattr(self, key, self.config.__dict__[key])
-        
+
     def __str__(self):
         return self.username
-        
+
     def __repr__(self):
         return "<User \"" + self.username + "\">"
